af7e316d6cbf96b5b349a6e8c449f2c63d2c5954
SaltedFish985 <51524862+SaltedFish985@users.noreply.github.com>
2019-06-25 14:45:35 +0800

    Update challenge6.py

--- challenge6.py
+++ challenge6.py
@@ -11,17 +11,14 @@ def combine():
     the_combine = the_xlsx.create_sheet('combine')
     for column in the_stutents["A:C"]:
         for cell in column:
-            position = ''.join((cell.column, str(cell.row)))
-            the_combine[position] = cell.value
+            the_combine[cell.column + str(cell.row)] = cell.value
     the_column = "B"
     for row1 in range(2,486):            
         for row2 in range(2,486):
-            if the_combine[''.join((the_column, str(row1)))].value == the_time[''.join((the_column, str(row2)))].value:
-                position1 = ''.join(("D", str(row1)))
-                position2 = ''.join(("C", str(row2)))
-                the_combine[position1].value = the_time[position2].value
+            if the_combine[the_column + str(row1)].value == the_time[the_column + str(row2)].value:
+                the_combine["D" + str(row1)].value = the_time["C" + str(row2)].value
     the_combine["D1"] = "学习时间"
-    the_xlsx.save('/home/shiyanlou/Code/courses.xlsx')       
+    the_xlsx.save('/home/shiyanlou/Code/courses.xlsx')
 
 def split():
     the_year = []
@@ -29,7 +26,7 @@ def split():
     the_combine = the_xlsx.get_sheet_by_name('combine')
     the_column = "A"
     for row in range(2, 486):
-        the_year.append(the_combine[''.join((the_column, str(row)))].value.year)
+        the_year.append(the_combine[the_column + str(row)].value.year)
     years = list(set(the_year))
     years.sort()
     for each_year in years:
@@ -38,11 +35,11 @@ def split():
         ws1.title = str(each_year)
         that_row = 2
         for row in range(2, 486):
-            if each_year == the_combine[''.join((the_column, str(row)))].value.year:                
-                ws1[''.join(("A", str(that_row)))].value = the_combine[row][0].value
-                ws1[''.join(("B", str(that_row)))].value = the_combine[row][1].value
-                ws1[''.join(("C", str(that_row)))].value = the_combine[row][2].value
-                ws1[''.join(("D", str(that_row)))].value = the_combine[row][3].value
+            if each_year == the_combine[the_column + str(row)].value.year:                
+                ws1["A" + str(that_row)].value = the_combine[row][0].value
+                ws1["B" + str(that_row)].value = the_combine[row][1].value
+                ws1["C" + str(that_row)].value = the_combine[row][2].value
+                ws1["D" + str(that_row)].value = the_combine[row][3].value
                 that_row += 1
         ws1["A1"] = "创建时间"
         ws1["B1"] = "课程名称"
