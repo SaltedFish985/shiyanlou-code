2e5053eed8bcdf8e8e14441559a78d2f4833298e
SaltedFish985 <124588840@qq.com>
2019-06-30 13:43:01 +0800

    commit challenge10

---
+++ challenge10/app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, url_for, redirect
+import os
+import json
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/challenge'
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref=db.backref('files'))
+    content = db.Column(db.Text)
+    def __init__(self, title, created_time, category_id, content):
+        self.title = title
+        self.created_time = created_time
+        self.category = category_id
+        self.content = content
+    def __repr__(self):
+        return  '<File(name=%s)>' % self.name
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return  '<File(name=%s)>' % self.name
+
+db.create_all()
+java = Category('Java')
+python = Category('Python')
+file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
+file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
+db.session.add(java)
+db.session.add(python)
+db.session.add(file1)
+db.session.add(file2)
+db.session.commit()
+
+@app.route('/')
+def index():
+    title_list = []
+    the_data = {}
+    the_files = File.query.all()    
+    for the_file in the_files:
+        the_data['id'] = the_file.id
+        the_data['title'] = the_file.title        
+        title_list.append(the_data)
+        the_data = {}
+    return render_template('index.html', titles=title_list)
+
+@app.route('/files/<int:file_id>')
+def file(file_id):
+    the_files = File.query.all()
+    id_list = []
+    dict_data = {}
+    for the_file in the_files:
+        id_list.append(the_file.id)
+    if file_id in id_list:
+        one_file = File.query.get(file_id)
+        dict_data['content'] = one_file.content
+        dict_data['created_time'] = one_file.created_time
+        the_category = Category.query.get(one_file.category_id)
+        dict_data['category'] = the_category.name
+        return render_template('file.html', the_data=dict_data)
+    else:        
+        return redirect('not_found')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', the_str='shiyanlou 404'), 404
+
+if __name__ == '__main__':
+    app.run()
