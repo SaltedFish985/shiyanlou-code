4fb189eae4d1c1de149759c43c5a0366f8effc03
SaltedFish985 <124588840@qq.com>
2019-07-21 13:25:59 +0800

    challenge24

---
+++ challenge24.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def quarter_volume():
+    data = pd.read_csv('apple.csv', header=0)
+    new_data = pd.Series(list(data['Volume']), index=pd.to_datetime(list(data['Date'])))
+    result = new_data.resample('Q').sum()
+    sort_result = result.sort_values(ascending=False)
+    second_volume = sort_result[1]
+    return second_volume
+
+if __name__ == '__main__':
+    print(quarter_volume())
